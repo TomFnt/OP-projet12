30f289a545896570a1ed0877b9cc13d055878ef9
TomFnt <tom.fnt9@gmail.com>
2024-08-21 21:07:48 +0200

    Re-use JsonResponse in TipsController & add endpoint "api" before endpoint "tips"

--- src/Controller/TipsController.php
+++ src/Controller/TipsController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TipsController extends AbstractController
 {
-    #[Route('/tips', name: 'app_tips')]
-    public function index(): Response
+    #[Route('/api/tips', name: 'app_tips')]
+    public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
