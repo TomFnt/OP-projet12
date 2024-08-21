c40b5d05e10853b1360399351c5e1af7a3855311
TomFnt <tom.fnt9@gmail.com>
2024-08-21 22:22:10 +0200

    get all tips and return in json response in api/tips route

--- src/Controller/TipsController.php
+++ src/Controller/TipsController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\TipsRepository;
+use Masterminds\HTML5\Serializer\RulesInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class TipsController extends AbstractController
 {
     #[Route('/api/tips', name: 'app_tips')]
-    public function index(): JsonResponse
+    public function getTips(TipsRepository $tipsRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TipsController.php',
-        ]);
+        $tips_list = $tipsRepository->findAll();
+
+        if($tips_list){
+            $json_response = $serializer->serialize($tips_list, 'json');
+            return new JsonResponse($json_response, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
