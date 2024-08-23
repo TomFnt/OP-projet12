f328fa02e7f06d5ac72c403f8cad6a3f500ceafc
TomFnt <tom.fnt9@gmail.com>
2024-08-23 18:24:38 +0200

    Create api/user route

---
+++ src/Controller/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class UserController extends AbstractController
+{
+    #[Route('api/user', name: 'app_user_create',methods: ['POST'])]
+    public function createUser(
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher
+    ): JsonResponse
+    {
+       $user = $serializer->deserialize($request->getContent(), User::class, 'json' );
+
+       // get user password & hash them before user creation
+        $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hashedPassword);
+
+        $user->setRoles(['ROLE_USER']);
+
+       $em->persist($user);
+       $em->flush();
+
+        return $this->json($user, Response::HTTP_OK, [], (array)'serializer');
+
+    }
+}
