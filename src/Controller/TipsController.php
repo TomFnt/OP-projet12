256946dfa6bf996aa0ecb17461089ceddcf2ea3e
TomFnt <tom.fnt9@gmail.com>
2024-08-27 01:29:26 +0200

    add missing Public access control for api/auth route & add isGranted annotation for api/tips & api/tip

--- src/Controller/TipsController.php
+++ src/Controller/TipsController.php
@@ -10,17 +10,20 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class TipsController extends AbstractController
 {
+    #[IsGranted('ROLE_USER', message: 'Vous devez vous connecter pour accèder à cette route')]
     #[Route('/api/tips', name: 'app_tips', methods: ['GET'])]
     public function getTips(TipsRepository $tipsRepository, SerializerInterface $serializer): JsonResponse
     {
         return $this->json($tipsRepository->findAll(), Response::HTTP_OK, [], (array) 'serializer');
     }
 
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'êtes pas autorisé à créer un nouveau conseil')]
     #[Route('/api/tip', name: 'app_tip_create', methods: ['POST'])]
     public function createTip(
         Request $request,
