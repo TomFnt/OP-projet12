4f0ccdcbb87cd927bb344285f5db2ed044fbd050
TomFnt <tom.fnt9@gmail.com>
2024-09-05 16:49:26 +0200

    remove header response from OpenWeatherMap where statut code == 200

--- src/Service/WeatherForecast.php
+++ src/Service/WeatherForecast.php
@@ -2,30 +2,44 @@
 
 namespace App\Service;
 
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-
 
 class WeatherForecast
 {
     private HttpClientInterface $httpClient;
     private string $apiKey;
 
-    public function __construct(HttpClientInterface $httpClient, string $apiKey)
+    public function __construct(HttpClientInterface $httpClient, string $apiKey, TagAwareCacheInterface $cachePool)
     {
         $this->httpClient = $httpClient;
         $this->apiKey = $apiKey;
+        $this->cachePool = $cachePool;
     }
 
+    public function getWeatherForecast(string $town): array
+    {
+        return $this->callOpenWeatherMap($town);
 
-    public function getWeatherForecastByTown(string $town): array
+
+    }
+
+    public function callOpenWeatherMap(string $town)
     {
         $url = sprintf(
-            'http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric',
+            'http://api.openweathermap.org/data/2.5/forecast?q=%s&appid=%s&units=metric',
             urlencode($town),
             $this->apiKey
         );
 
         $response = $this->httpClient->request('GET', $url);
-        return $response->toArray();
+
+        if ($response->getStatusCode() !== 200) {
+            return null;
+        }
+
+        $response = $response->toArray();
+
+        return $response["list"];
     }
 }
