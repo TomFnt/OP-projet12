ff9e98bd89fecf183fd24ebbaa9c114cb193af89
TomFnt <tom.fnt9@gmail.com>
2024-09-11 22:48:38 +0200

    change weather forecast route api for french usage & translate error message in l34

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -14,12 +14,14 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class WeatherController extends AbstractController
 {
     #[IsGranted('ROLE_USER', message: 'Vous devez vous connecter pour accèder à cette route')]
-    #[Route('api/weather/{town}', name: 'app_weather_by_town', methods: ['GET'])]
-    #[Route('api/weather', name: 'app_weather_nullable', methods: ['GET'])]
+    #[Route('api/meteo/{town}', name: 'app_weather_by_town', methods: ['GET'])]
+    #[Route('api/meteo', name: 'app_weather_nullable', methods: ['GET'])]
     public function getWeatherForecast(
         WeatherForecast $weatherForecast,
         string $town = ''
     ): JsonResponse {
+
+       /*get town specify in User account if user does'nt set in their api call a town */
         if ('' == $town) {
             $town = $this->getUser()->getCity();
         }
